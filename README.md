d81d12daef6ea51b887d7baa048d0369e9248d3c
Michael Maier <maier1michael@gmail.com>
2016-11-02 16:23:25 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,23 @@
+# Cycle.js storageify
+
+Augments your Cycle.js component (main function) by storing its onion-shaped state in local storage.
+
+## Quick example
+
+```js
+import onionify from 'cycle-onionify';
+import storageify from 'cycle-storageify';
+import storageDriver from '@cycle/storage';
+// ...
+
+const wrappedMain = onionify(storageify(main, {key: 'my-local-storage-key'}));
+
+Cycle.run(wrappedMain, {
+  DOM: makeDOMDriver('#app'),
+  storage: storageDriver,
+});
+```
+
+## Advanced example
+
+See `storageify` branch of [TodoMVC]().
---
+++ src/index.ts
@@ -0,0 +1,88 @@
+import xs, {Stream} from 'xstream';
+
+export type Component<Sources, Sinks> = (sources: Sources) => Sinks;
+export type Reducer = (state: any) => any;
+
+export interface OnionSource {
+  onion: any
+}
+
+export interface OnionSink {
+  onion: any
+}
+
+export interface StorageSource {
+  storage: any
+}
+
+export interface StorageSink {
+  storage: Stream<Reducer>
+}
+
+function serialize(state: any) {
+  return JSON.stringify(state);
+}
+
+function deserialize(str: string) {
+  return JSON.parse(str) || {};
+}
+
+export default function storageify<Sources extends OnionSource, Sinks extends OnionSink>(
+      Component: Component<Sources, Sinks>,
+      options: {
+          key: string,
+          serialize(state: any): string,
+          deserialize(stateStr: string): any,
+        }
+    ): Component<Sources & StorageSource, Sinks & StorageSink> {
+  const _options =
+    Object.assign(
+        // defaults
+        {
+          key: 'storageify',
+          serialize,
+          deserialize,
+        },
+        options
+      );
+  return function (sources: Sources & StorageSource): Sinks & StorageSink {
+    const localStorage$ = sources.storage.local.getItem(_options.key).take(1);
+    const storedData$ = localStorage$.map(_options.deserialize);
+    const state$ = sources.onion.state$;
+    const componentSinks = Component(sources);
+
+    // change initial reducer (first reducer) of component
+    // to merge default state with stored state
+    const childReducer$ = componentSinks.onion;
+    const initialReducer$ =
+      xs.combine(
+          storedData$,
+          childReducer$.take(1)
+        )
+        .map(([storedState, initialReducerChild]: [any, Reducer]) =>
+            prevState =>
+              Object.assign(
+                  initialReducerChild(prevState),
+                  storedState
+                )
+          );
+    // replace initial reducer
+    const parentReducer$ =
+      xs.merge(
+          initialReducer$,
+          childReducer$.drop(1)
+        );
+
+    const storage$ = state$.map(_options.serialize)
+        .map(value => ({key: _options.key, value}));
+    const sinks =
+      Object.assign(
+          componentSinks,
+          {
+            onion: parentReducer$,
+            storage: storage$,
+          }
+        );
+    return sinks;
+  };
+}
